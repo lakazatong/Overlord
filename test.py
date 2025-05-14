b6256e3a87a1a7af522079def66db99e310aa9e0
lakazatong <Bo_wood@outlook.fr>
2025-05-14 15:45:29 +0200

    process chapter, page

--- test.py
+++ test.py
@@ -1,17 +1,12 @@
-import time, math, os, shutil, numpy as np
+import cProfile, time, math, os, shutil, numpy as np
 from PIL import Image
-from bisect import insort
-import matplotlib.pyplot as plt
+# from bisect import insort
 
+import matplotlib.pyplot as plt
 plt.figure(figsize=(12, 4))
 
-# import cProfile
 from manga_ocr import MangaOcr
-
-# image_path = "./1/097.png"
-image_path = "./1/111.png"
-# image_path = "./1/117.png"
-# image_path = "./1/128.png"
+ocr = MangaOcr()
 
 colors_rgb = [(230, 25, 75), (60, 180, 75), (255, 225, 25), (67, 99, 216), (245, 130, 49), (145, 30, 180), (70, 240, 240), (240, 50, 230), (188, 246, 12), (250, 190, 190), (0, 128, 128), (230, 190, 255), (154, 99, 36), (255, 250, 200), (128, 0, 0), (170, 255, 195), (128, 128, 0), (255, 216, 177), (0, 0, 117), (128, 128, 128)]
 
@@ -28,53 +23,12 @@ def get_circular_mask(radius):
 		masks_cache[radius] = circular_mask(radius)
 	return masks_cache[radius]
 
-def circle_offsets(radius):
-	offsets = []
-	r2 = radius * radius
-	for dx in range(-radius, radius + 1):
-		for dy in range(-radius, radius + 1):
-			d2 = dx*dx + dy*dy
-			if d2 <= r2:
-				insort(offsets, ((-d2), (dx, dy)))
-	return [xy for _, xy in offsets]
-
-circle_offsets_cache = {}
-
-def get_circle_offsets(radius):
-	if radius not in circle_offsets_cache:
-		circle_offsets_cache[radius] = circle_offsets(radius)
-	return circle_offsets_cache[radius]
-
-def generate_coordinates_within_distance(coord, radius, shape):
-	x, y = coord
-	offsets = get_circle_offsets(radius)
-	return [
-		(x + dx, y + dy)
-		for dx, dy in offsets
-		if 0 <= x + dx < shape[0] and 0 <= y + dy < shape[1]
-	]
-
-# radius = 100
-# shape = (radius*2+1, radius*2+1, 3)
-# outer = math.floor(5*radius*math.sqrt(2))
-# cur_outer = 0
-# arr = np.zeros(shape, dtype=np.uint8)
-# for x, y, d in generate_coordinates_within_distance((radius, radius), radius, shape):
-# 	if cur_outer < outer:
-# 		arr[x, y] = (255, 255, 255)
-# 	else:
-# 		arr[x, y] = (128, 128, 128)
-# 	cur_outer += 1
-# Image.fromarray(arr).save("circle.png")
-
-# exit(0)
-
 def label_groups(binary_image, radius=20):
 	shape = binary_image.shape
 	group_arr = np.full_like(binary_image, -1, dtype=int)
 	group_arr[binary_image == 1] = -2
 	group_id = 0
-	outer = 4*math.floor(radius*math.sqrt(2))
+	outer_count = 4*math.floor(radius*math.sqrt(2))
 	mask = get_circular_mask(radius)
 
 	def help(start_x, start_y):
@@ -97,9 +51,12 @@ def label_groups(binary_image, radius=20):
 
 		while queue:
 			cx, cy = queue.pop()
+			outer_counter = 0
 			for nx, ny in apply_mask(cx, cy):
 				group_arr[nx, ny] = group_id
-				queue.append((nx, ny))
+				if outer_counter < outer_count:
+					queue.append((nx, ny))
+				outer_counter += 1
 
 	while np.any(group_arr == -2):
 		# print(group_id)
@@ -110,14 +67,11 @@ def label_groups(binary_image, radius=20):
 	
 	return group_arr
 
-def extract_bounding_boxes(group_arr, spacing_threshold=5, min_width=23):
+def extract_boxes(group_arr, min_area=200):
 	boxes = {}
-	max_gid = 0
 	for y in range(group_arr.shape[0]):
 		for x in range(group_arr.shape[1]):
-			gid = group_arr[y, x]
-			if gid > max_gid:
-				max_gid = gid
+			gid = int(group_arr[y, x])
 			if gid != -1:
 				if gid not in boxes:
 					boxes[gid] = [x, x + 1, y, y + 1]
@@ -126,11 +80,12 @@ def extract_bounding_boxes(group_arr, spacing_threshold=5, min_width=23):
 					boxes[gid][1] = max(boxes[gid][1], x + 1)
 					boxes[gid][2] = min(boxes[gid][2], y)
 					boxes[gid][3] = max(boxes[gid][3], y + 1)
+	
+	return {gid: box for gid, box in boxes.items() if (box[1] - box[0]) * (box[3] - box[2]) > min_area}
 
-	group_mask = np.where(group_arr != -1, 1, 0)
-	col_averages = np.mean(group_mask, axis=0)
-
-	split_boxes = []
+def split_boxes(boxes, group_arr, col_averages, spacing_threshold=5, min_width=23):
+	splitted = {}
+	max_gid = max(boxes.keys())
 	for gid, (x1, x2, y1, y2) in boxes.items():
 		local_group_mask = np.where(group_arr == gid, 1, 0)
 		col_avg = col_averages[x1:x2]
@@ -150,12 +105,8 @@ def extract_bounding_boxes(group_arr, spacing_threshold=5, min_width=23):
 			zero_ranges.append((start, len(col_avg)))
 
 		if not zero_ranges:
-			split_boxes.append((gid, (x1, x2, y1, y2)))
+			splitted[gid] = (x1, x2, y1, y2)
 			continue
-
-		plt.clf()
-		plt.plot(col_avg)
-		plt.savefig(f"groups/{max_gid + 1:02}_plot.png")
 
 		mids = [x1 + (start + end) // 2 for start, end in zero_ranges]
 		split_xs = [x1] + mids + [x2]
@@ -175,16 +126,27 @@ def extract_bounding_boxes(group_arr, spacing_threshold=5, min_width=23):
 				print("impossible case reached")
 				exit(1)
 
-			new_x1 = start + col_nonzero[0]
-			new_x2 = start + col_nonzero[-1] + 1
-			new_y1 = y1 + row_nonzero[0]
-			new_y2 = y1 + row_nonzero[-1] + 1
+			new_x1 = int(start + col_nonzero[0])
+			new_x2 = int(start + col_nonzero[-1] + 1)
+			new_y1 = int(y1 + row_nonzero[0])
+			new_y2 = int(y1 + row_nonzero[-1] + 1)
 
 			max_gid += 1
-			split_boxes.append((max_gid, (new_x1, new_x2, new_y1, new_y2)))
+			splitted[max_gid] = (new_x1, new_x2, new_y1, new_y2)
 			i += 1
 
-	return sorted(split_boxes, key=lambda x: x[0])
+	return splitted
+
+def merge_boxes(boxes):
+	merged = []
+	for gid, (x1, x2, y1, y2) in sorted(boxes.items(), key=lambda item: item[1][0]):
+		for mgid, (mx1, mx2, my1, my2) in enumerate(merged):
+			if not (x2 < mx1 or x1 > mx2):
+				merged[mgid] = (min(x1, mx1),max(x2, mx2),min(y1, my1),max(y2, my2))
+				break
+		else:
+			merged.append((x1, x2, y1, y2))
+	return merged
 
 def draw_rectangle_on_numpy_arr(img_arr, offsets, color=[255, 0, 0]):
 	x1, x2, y1, y2 = offsets
@@ -200,34 +162,68 @@ def create_rgb_image(group_arr, colors_rgb, boxes):
 			group_id = group_arr[i, j]
 			if group_id != -1:
 				rgb_arr[i, j] = colors_rgb[group_id % len(colors_rgb)]
-	for _, offsets in boxes:
+	for offsets in boxes:
 		draw_rectangle_on_numpy_arr(rgb_arr, offsets)
 	return rgb_arr
 
-def main():
-	if os.path.exists("groups"):
-		shutil.rmtree("groups")
-	os.makedirs("groups")
+def process(chapter, page):
+	page_folder = f"./{chapter}/{page}"
+	groups_folder = f"{page_folder}/groups"
+	
+	if os.path.exists(page_folder):
+		shutil.rmtree(page_folder)
+	os.makedirs(groups_folder)
 
-	arr = np.array(Image.open(image_path).convert("L"))
+	arr = np.array(Image.open(f"{page_folder}.png").convert("L"))
 	binary_image = (arr != 255).astype(np.uint8)
+
+	# start_time = time.time()
+
 	group_arr = label_groups(binary_image)
-	boxes = extract_bounding_boxes(group_arr)
+	col_averages = np.mean(np.where(group_arr != -1, 1, 0), axis=0)
+	raw_boxes = extract_boxes(group_arr)
+	boxes = []
+	if len(raw_boxes) > 0:
+		splitted_boxes = split_boxes(raw_boxes, group_arr, col_averages)
+		boxes = merge_boxes(splitted_boxes)
+		boxes = sorted(boxes, key=lambda b: -b[0])
 
-	for gid, (x1, x2, y1, y2) in boxes:
-		print(gid, x2 - x1)
+	# print(time.time() - start_time)
 
-	# return
+	for gid, (x1, x2, y1, y2) in enumerate(boxes):
+		# print(gid, x2 - x1, y2 - y1)
+		plt.clf()
+		col_avg = col_averages[x1:x2]
+		plt.plot(col_avg)
+		plt.axhline(y=col_avg.mean(), color='red')
+		plt.savefig(f"{groups_folder}/{gid:02}_plot.png")
 
-	Image.fromarray(create_rgb_image(group_arr, colors_rgb, boxes)).save("groups.png")
-	ocr = MangaOcr()
+	Image.fromarray(create_rgb_image(group_arr, colors_rgb, boxes)).save(f"{page_folder}/groups.png")
 
-	for i, (gid, (x1, x2, y1, y2)) in enumerate(boxes):
+	for gid, (x1, x2, y1, y2) in enumerate(boxes):
 		crop_img = Image.fromarray(np.pad(arr[y1:y2, x1:x2], ((15, 15), (15, 15)), mode='constant', constant_values=255))
-		crop_img.save(f"groups/{gid:02}.png")
+		crop_img.save(f"{groups_folder}/{gid:02}.png")
 		text = ocr(crop_img)
-		with open(f"groups/{gid:02}.txt", "w", encoding="utf-8") as f:
+		with open(f"{groups_folder}/{gid:02}.txt", "w", encoding="utf-8") as f:
 			f.write(text)
 
+def main():
+	for chapter in range(1, 5+1):
+		for page_filename in os.listdir(f"./{chapter}"):
+			if not page_filename.endswith(".png"):
+				continue
+			page = page_filename.split(".")[0]
+
+			if int(page) < 8:
+				continue
+
+			process(chapter, page)
+			print(chapter, page)
+
 # cProfile.run('main()')
+
 main()
+# process(1, 97)
+# process(1, 111)
+# process(1, 117)
+# process(1, 128)
